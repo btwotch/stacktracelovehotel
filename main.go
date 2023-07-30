02a12049232163f879b0c90c54f2aaf5d4fe3db5
btwotch <btwotch+github@gmail.com>
2023-07-30 21:34:26 +0200

    stacktraces: add struct to manage stacktraces

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
-	"regexp"
-	"strconv"
 )
 
 func main() {
@@ -14,7 +11,7 @@ func main() {
 		fmt.Printf("usage: %s [filename]\n", os.Args[0])
 	}
 
-	fmt.Printf("%s", dotPreamble("graph"))
+	var sts stacktraces
 
 	for _, filename := range os.Args[1:] {
 
@@ -23,52 +20,10 @@ func main() {
 			panic(err)
 		}
 
-		st := parseStacktrace(b)
-		st.name = filepath.Base(filename)
+		name := filepath.Base(filename)
+		sts.addFromString(name, b)
 
-		//		st.ToDot()
-		fmt.Printf("%s", st.dotRelations())
 	}
 
-	fmt.Printf("%s", dotClosing())
-}
-
-func parseStacktrace(b []byte) stacktrace {
-	var err error
-
-	goroutineRegex := regexp.MustCompile("^goroutine ([0-9]+).*:$")
-	functionRegex := regexp.MustCompile(`(.*)\.(.*)\(.*\)`)
-	lineRegex := regexp.MustCompile(`\s*(\/.*\.go):(\d+)`)
-
-	var st stacktrace
-	var fn *function
-	for _, lineBytes := range bytes.Split(b, []byte("\n")) {
-		line := string(lineBytes)
-		match := goroutineRegex.FindStringSubmatch(line)
-		if len(match) == 2 {
-			st.goroutine, err = strconv.ParseUint(match[1], 10, 64)
-			if err != nil {
-				panic(err)
-			}
-			continue
-		}
-		match = functionRegex.FindStringSubmatch(line)
-		if len(match) == 3 {
-			fn = &function{}
-			fn.packageName = match[1]
-			fn.functionName = match[2]
-			continue
-		}
-		match = lineRegex.FindStringSubmatch(line)
-		if len(match) == 3 {
-			fn.path = match[1]
-			fn.line, err = strconv.ParseUint(match[2], 10, 64)
-			if err != nil {
-				panic(err)
-			}
-			st.appendFunction(*fn)
-			continue
-		}
-	}
-	return st
+	fmt.Printf("%s\n", sts.String())
 }
--- stacktrace.go
+++ stacktrace.go
@@ -1,6 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type stacktraces struct {
+	sts []stacktrace
+}
+
+func (sts *stacktraces) String() string {
+	str := dotPreamble("Stacktraces")
+
+	for _, st := range sts.sts {
+		str += st.dotRelations()
+		str += "\n"
+	}
+
+	str += dotClosing()
+
+	return str
+}
+
+func (sts *stacktraces) addFromString(name string, b []byte) {
+	st := parseStacktrace(b)
+	st.name = name
+
+	sts.sts = append(sts.sts, st)
+}
 
 type stacktrace struct {
 	name      string
@@ -64,4 +94,49 @@ func (st *stacktrace) dotRelations() string {
 
 func (st *stacktrace) appendFunction(fn function) {
 	st.fs = append(st.fs, fn)
+}
+
+func parseStacktrace(b []byte) stacktrace {
+	var err error
+
+	goroutineRegex := regexp.MustCompile("^goroutine ([0-9]+).*:$")
+	functionRegex := regexp.MustCompile(`([^/]*)\.([^/]*)\(.*\)`)
+	lineRegex := regexp.MustCompile(`\s*(\/.*\.go):(\d+)`)
+
+	var st stacktrace
+	var fn *function
+	for _, lineBytes := range bytes.Split(b, []byte("\n")) {
+		line := string(lineBytes)
+		match := goroutineRegex.FindStringSubmatch(line)
+		if len(match) == 2 {
+			st.goroutine, err = strconv.ParseUint(match[1], 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			continue
+		}
+		match = functionRegex.FindStringSubmatch(line)
+		if len(match) == 3 {
+			fn = &function{}
+			fn.packageName = match[1]
+			fn.functionName = match[2]
+			continue
+		}
+		match = lineRegex.FindStringSubmatch(line)
+		if len(match) == 3 {
+			fn.path = match[1]
+			fn.line, err = strconv.ParseUint(match[2], 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			if !fn.Empty() {
+				st.appendFunction(*fn)
+			}
+			fn = &function{}
+			continue
+		}
+
+	}
+
+	return st
 }
